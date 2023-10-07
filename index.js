9d42966fcbb12e7be4f5b611160751923ff47dc0
cschuitevoerder <144602955+cschuitevoerder@users.noreply.github.com>
2023-10-07 12:49:48 +0100

    Reconfigure HTML, CSS and JS
    Replaced bootstrap card forecast functionality, added new forecast API calls, removed redundant CSS code.

--- index.js
+++ index.js
@@ -21,9 +21,45 @@ function formatDate(timestamp) {
   return `${day}: ${hours}: ${minutes}`;
 }
 
-function displayWeatherCondition(response) {
-  console.log(response);
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[day];
+}
 
+function displayWeatherForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#weather-forecast");
+  let forecastHTML = `<div class="row">`;
+  let icon = forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+        <div class="weather-forecast-tempreture">
+         <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+           forecastDay.condition.icon
+         }.png" alt="" id="forecast-icon" class="forecastIcon"/>
+         </br>
+          <span class="weather-forecast-tempreture-max">${Math.round(
+            forecastDay.temperature.minimum
+          )}°</span>
+          <span class="weather-forecast-tempreture-divider">|</span>
+          <span class="weather-forecast-tempreture-min">${Math.round(
+            forecastDay.temperature.maximum
+          )}°</span>
+        </div>
+    </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function displayWeatherCondition(response) {
   let cityElement = document.querySelector("h1");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
@@ -42,7 +78,17 @@ function displayWeatherCondition(response) {
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
+
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  getForecast(response.data.coordinates);
+}
+
+function getForecast(coordinates) {
+  let apiKey = `b7ft5f752043d3f03c584f3o225b20a4`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherForecast);
 }
 
 function searchCity(city) {
@@ -92,4 +138,4 @@ farenheightLink.addEventListener("click", farChange);
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", celsiusChange);
 
-searchCity("new york");
+searchCity("London");
